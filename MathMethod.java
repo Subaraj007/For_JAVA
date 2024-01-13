7516f9b36c9f02855f25f53c29e8b4c96c513f99
Subaraj <subaraj1961@gmail.com>
2024-01-13 15:32:20 +0530

    MathMethod

---
+++ MathMethod.java
@@ -0,0 +1,9 @@
+public class MathMethod {
+    public static void main(String[] args) {
+        double x = 0.5;
+        double results = Math.acos(x);
+        System.out.println(results);
+
+    }
+
+}
