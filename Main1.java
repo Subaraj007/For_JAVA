d456c3a08ba8fb5442ffcdb872b7122fe7cf445e
Subaraj <subaraj1961@gmail.com>
2024-01-17 09:28:56 +0530

    Polymorphism

---
+++ Main1.java
@@ -0,0 +1,10 @@
+class Main1 {
+    public static void main(String[] args) {
+        Animal myAnimal = new Animal();
+        Animal myDog = new Dog();
+        Animal myPig = new Pig();
+        myAnimal.animalSound();
+        myDog.animalSound();
+        myPig.animalSound();
+    }
+}
