72e3b9e8393153a12fdfc41c0a0af3b96e6f0318
Subaraj <subaraj1961@gmail.com>
2024-01-28 21:25:44 +0530

    Palindrome

---
+++ Palindrome.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+@SuppressWarnings("all")
+public class Palindrome {
+    public static void main(String[] args) {
+        Scanner myInput = new Scanner(System.in);
+        System.out.println("Enter user name: ");
+        String userInput = myInput.nextLine();
+        StringBuffer sBuffer = new StringBuffer(String.valueOf(userInput));
+        StringBuffer rev = sBuffer.reverse();
+        System.out.println(rev);
+        String revString = rev.toString();
+        if (userInput.equals(revString)) {
+            System.out.println("This is palindrome.");
+
+        } else {
+            System.out.println("This is not palindrome.");
+        }
+
+    }
+}
