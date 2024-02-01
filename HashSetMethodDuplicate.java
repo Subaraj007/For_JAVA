a6cb388559a0d99f46e6668add08ecddf6d22bb6
Subaraj <subaraj1961@gmail.com>
2024-02-01 16:46:21 +0530

    HashSetMethodDuplicate

---
+++ HashSetMethodDuplicate.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+
+/**
+ * @author Subaraj
+ */
+public class HashSetMethodDuplicate {
+    public static void main(String[] args) {
+        String[] a1 = { "Java", "Python", "Java", "Html", "Css" };
+        HashSet<String> duplicateString = new HashSet<>();
+        for (String i : a1) {
+            if (duplicateString.add(i) == false) {
+                System.out.println("Duplicate is " + i);
+            }
+        }
+
+    }
+}
