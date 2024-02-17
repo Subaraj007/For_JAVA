ad6332de91f6c4d700675610e3df1970f244d213
Subaraj <subaraj1961@gmail.com>
2024-02-17 20:03:54 +0530

    Push and Pop

---
+++ dsa/StackFunc.java
@@ -0,0 +1,12 @@
+package dsa;
+
+@SuppressWarnings("all")
+public class StackFunc {
+    public static void main(String[] args) {
+        Stack s1 = new Stack();
+        s1.push(4);
+        s1.push(6);
+        System.out.println(s1);
+
+    }
+}
